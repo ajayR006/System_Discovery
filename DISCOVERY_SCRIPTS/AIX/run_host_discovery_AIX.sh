b989093a7ba28b8bd03c82fdd93bc830f978310a
Vimal Kumar <Vimal.Kumar@ril.com>
2020-07-10 06:48:30 +0000

    Added run_host_discovery_AIX.sh

---
+++ DISCOVERY_SCRIPTS/AIX/run_host_discovery_AIX.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+##
+
+PTH="/opt/ngoagent/JAM/Discovery/script/";
+source ${PTH}JAM_CONFIG.conf
+
+rm -f ${PTH}JAM_success_*.csv
+
+ipp=`prtconf|grep -i "IP Address:"|awk -F":" '{print $2}'|head -1`
+
+echo $ipp > $LOGERR
+/usr/bin/bash ${PTH}host_discovery_AIX.sh 1> $OUTCSV 2>> $LOGERR
