38ddbbacf86bd501d24da5a880a1707969856b7a
Vimal Kumar <Vimal.Kumar@ril.com>
2020-07-10 06:51:01 +0000

    Added run_host_discovery_SUNOS.sh

---
+++ DISCOVERY_SCRIPTS/SunOS/run_host_discovery_SUNOS.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+##
+
+PTH="/opt/ngoagent/JAM/Discovery/script/";
+source ${PTH}JAM_CONFIG.conf
+
+rm -f ${PTH}JAM_success_*.csv
+
+ipp=`/usr/sbin/ifconfig -a | awk '/flags/ {printf $1" "} /inet/ {print $2}' | grep -v lo | head -1 | awk -F':' '{print $2}' | sed 's/^ *//g'`
+
+echo $ipp > $LOGERR
+/usr/bin/bash ${PTH}host_discovery_SUNOS.sh 1> $OUTCSV 2>> $LOGERR
