0dd57633a62ff571a53ab9c9b42cb34f4811f7e0
Vimal Kumar <Vimal.Kumar@ril.com>
2020-07-20 07:28:29 +0000

    Updated run_host_discovery_SUNOS.sh

--- DISCOVERY_SCRIPTS/SunOS/run_host_discovery_SUNOS.sh
+++ DISCOVERY_SCRIPTS/SunOS/run_host_discovery_SUNOS.sh
@@ -9,4 +9,4 @@ rm -f ${PTH}JAM_success_*.csv
 ipp=`/usr/sbin/ifconfig -a | awk '/flags/ {printf $1" "} /inet/ {print $2}' | grep -v lo | head -1 | awk -F':' '{print $2}' | sed 's/^ *//g'`
 
 echo $ipp > $LOGERR
-/usr/bin/bash ${PTH}host_discovery_SUNOS.sh 1> $OUTCSV 2>> $LOGERR
+bash ${PTH}host_discovery_SUNOS.sh 1> $OUTCSV 2>> $LOGERR
