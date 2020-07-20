cc6cea66f84414528a166f162c57fe25d4acd993
Vimal Kumar <Vimal.Kumar@ril.com>
2020-07-20 07:27:51 +0000

    Updated run_host_discovery_AIX.sh

--- DISCOVERY_SCRIPTS/AIX/run_host_discovery_AIX.sh
+++ DISCOVERY_SCRIPTS/AIX/run_host_discovery_AIX.sh
@@ -9,4 +9,4 @@ rm -f ${PTH}JAM_success_*.csv
 ipp=`prtconf|grep -i "IP Address:"|awk -F":" '{print $2}'|head -1`
 
 echo $ipp > $LOGERR
-/usr/bin/bash ${PTH}host_discovery_AIX.sh 1> $OUTCSV 2>> $LOGERR
+bash ${PTH}host_discovery_AIX.sh 1> $OUTCSV 2>> $LOGERR
