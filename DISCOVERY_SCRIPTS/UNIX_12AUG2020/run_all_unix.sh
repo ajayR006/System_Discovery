74c87d2af486a2b1866f40b8f478566266059d99
Vimal Kumar <Vimal.Kumar@ril.com>
2020-08-12 17:16:18 +0000

    Added run_all_unix.sh

---
+++ DISCOVERY_SCRIPTS/UNIX_12AUG2020/run_all_unix.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+##################################
+#            
+# RJIL
+# JAM Project
+# AUG-2020
+# vimal.kumar@ril.com
+# JAM Disocvery Script
+# wrapper file
+
+PTH="/opt/ngoagent/JAM/Discovery/script/";
+source ${PTH}JAM_CONFIG.conf
+rm -f ${PTH}JAM_success_*.csv
+
+CMD10=`which timeout|head -1`;
+
+ipp=`/sbin/ip r|grep -i src|awk '{print $9}' | head -1`
+
+$CMD10 30s bash ${PTH}run_checksum.sh
+
+echo $ipp > $LOGERR
+$CMD10 60s bash  ${PTH}all_unix.sh 1> $OUTCSV 2>> $LOGERR
+
+$CMD10 120s bash  ${PTH}all_unix_installed_SW.sh
